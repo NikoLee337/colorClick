00e1bfff90c16e10b813c3c7e479c6bc00749066
dlee107 <dlee107@uncc.edu>
2020-03-20 23:55:21 -0400

    Second Commit. Added codes that enables the play and instruction buttons to go to the next screen.

---
+++ app/src/main/java/com/example/colorclick/InstructionWindow.java
@@ -0,0 +1,14 @@
+package com.example.colorclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstructionWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction_window);
+    }
+}
--- app/src/main/java/com/example/colorclick/MainActivity.java
+++ app/src/main/java/com/example/colorclick/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.colorclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private ImageButton playbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        playbutton = (ImageButton) findViewById(R.id.playbutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               openInstructions();
+           }
+        });
+
+        playbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGameView();
+            }
+        });
+
+    }
+
+    //opens the game window view
+    public void openGameView() {
+        Intent intent = new Intent(this, GameView.class);
+        startActivity(intent);
+    }
+    //opens the instructions view window
+    public void openInstructions() {
+        Intent intent = new Intent (this, InstructionWindow.class);
+        startActivity(intent);
     }
 }
