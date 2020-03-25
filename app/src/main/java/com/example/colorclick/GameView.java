f2e0981a7aa71500c8472927457fcc9a57a17237
dlee107 <dlee107@uncc.edu>
2020-03-25 16:04:17 -0400

    FIFTH COMMIT: Added a temporary 5 sec timer to the Game View.

--- app/src/main/java/com/example/colorclick/GameView.java
+++ app/src/main/java/com/example/colorclick/GameView.java
@@ -4,16 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class GameView extends AppCompatActivity {
 
+    Handler handler;
+    Runnable runnable;
+    ProgressBar timer;
+    Random random;
 
+    int currentTime = 5000;
+    int startTime = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_view);
 
-    }
+        timer = findViewById(R.id.timer);
+
+        //set the initial timer to 5 seconds
+        timer.setMax(startTime);
+        timer.setProgress(startTime);
+
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                currentTime = currentTime - 1000;
+                timer.setProgress(currentTime);
+                if (currentTime > 0) {
+                    handler.postDelayed(runnable, 1000);
+                    }
+                }
+            };
+        handler.postDelayed(runnable, 1000);
+        }
+
+
 }
