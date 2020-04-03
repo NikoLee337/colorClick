2bb688dc8727eae4038c32f151c2f037df3aa41a
dlee107 <dlee107@uncc.edu>
2020-04-03 00:45:46 -0400

    Got to randomizer to sort of work

--- app/src/main/java/com/example/colorclick/GameView.java
+++ app/src/main/java/com/example/colorclick/GameView.java
@@ -21,9 +21,15 @@ import static com.example.colorclick.R.drawable.yellow1;
 public class GameView extends AppCompatActivity {
 
     //buttons
-    View btn_00, btn_01, btn_10, btn_11;
+    ImageButton btn_00, btn_01, btn_10, btn_11;
     ImageView toBeMatch;
     TextView level;
+
+    private final static int B_00 = 1;
+    private final static int B_01 = 2;
+    private final static int B_10 = 3;
+    private final static int B_11 = 4;
+    int bPosition = B_00;
 
     private final static int COLOR_RED = 1;
     private final static int COLOR_ORANGE = 2;
@@ -32,7 +38,7 @@ public class GameView extends AppCompatActivity {
     private final static int COLOR_BLUE = 5;
     private final static int COLOR_INDIGO = 6;
     private final static int COLOR_VIOLET = 7;
-    int MATCH_COLOR = 1;
+    int matchColor = 1;
 
     int currentLevel = 1;
 
@@ -40,7 +46,7 @@ public class GameView extends AppCompatActivity {
     Handler handler;
     Runnable runnable;
     ProgressBar timer;
-    Random r;
+    Random r1, r2;
     Dialog pause;
     View v;
 
@@ -54,35 +60,46 @@ public class GameView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-//        //color button ids
-        btn_00 = findViewById(R.id.button_00);
+        //color button ids
+//        btn_00 = findViewById(R.id.button_00);
 //        btn_01 = findViewById(R.id.button_01);
 //        btn_10 = findViewById(R.id.button_10);
 //        btn_11 = findViewById(R.id.button_11);
+
+        //color needed to be matched to other circles on the board
         toBeMatch = findViewById(R.id.imageView_color);
         toBeMatch.setClickable(false);
-//        //text variable for level
-//        level = findViewById(R.id.textView_level1);
+
+        //text variable for level
+        level = findViewById(R.id.textView_level1);
+
+        //display the level
+        level.setText("Level " + currentLevel);
+
+          //randomize the matched color
+        r1 = new Random();
+        matchColor = r1.nextInt(7) + 1;
+        setMatchColor(matchColor);
+
+        //generate board
+        generateGameBoard(matchColor);
+
+        //randomize buttons
+        r2 = new Random();
+        bPosition = r2.nextInt(4) + 1;
+
+
+//        //method that moves to the next level
+//        btn_00.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if ( btn_00.equals(toBeMatch)) {
+//                    btn_00.setVisibility(View. INVISIBLE);
 //
-//        //display the level
-//        level.setText("Level " + currentLevel);
-//
-//        //randomize the matched color
-//        r = new Random();
-//        MATCH_COLOR = r.nextInt(7) + 1;
-//        setMatchColor(MATCH_COLOR);
-
-        btn_00.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if ( btn_00.equals(toBeMatch)) {
-                    btn_00.setVisibility(View. INVISIBLE);
-
-
-                }
-                setContentView(R.layout.activity_game2);
-            }
-        });
+//                }
+//                setContentView(R.layout.activity_game2);
+//            }
+//        });
 
         /////////////////////////////////////////////////////////////
 
@@ -96,10 +113,8 @@ public class GameView extends AppCompatActivity {
         timer.setMax(startTime);
         timer.setProgress(startTime);
 
-        //handler for the timer
+        //handles the timer/progressbar countdown and main loop
         handler = new Handler();
-
-        //runs and handles the progressbar/timer
         runnable = new Runnable() {
             @Override
             public void run() {
@@ -124,38 +139,93 @@ public class GameView extends AppCompatActivity {
     }
 
     //display the random color for player to match
-//    private void setMatchColor (int startColor) {
-//        switch (startColor) {
-//            case COLOR_RED:
-//                toBeMatch.setImageResource(R.drawable.red1);
-//                currentLevel = COLOR_RED;
-//                break;
-//            case COLOR_ORANGE:
-//                toBeMatch.setImageResource(R.drawable.orange1);
-//                currentLevel = COLOR_ORANGE;
-//                break;
-//            case COLOR_YELLOW:
-//                toBeMatch.setImageResource(R.drawable.yellow1);
-//                currentLevel = COLOR_YELLOW;
-//                break;
-//            case COLOR_GREEN:
-//                toBeMatch.setImageResource(R.drawable.green1);
-//                currentLevel = COLOR_GREEN;
-//                break;
-//            case COLOR_BLUE:
-//                toBeMatch.setImageResource(R.drawable.blue1);
-//                currentLevel = COLOR_BLUE;
-//                break;
-//            case COLOR_INDIGO:
-//                toBeMatch.setImageResource(R.drawable.indigo1);
-//                currentLevel = COLOR_INDIGO;
-//                break;
-//            case COLOR_VIOLET:
-//                toBeMatch.setImageResource(R.drawable.violet1);
-//                currentLevel = COLOR_VIOLET;
-//                break;
-//        }
-//    }
+    private int setMatchColor (int match) {
+        switch (match) {
+            case COLOR_RED:
+                toBeMatch.setImageResource(R.drawable.red_footer);
+                matchColor = COLOR_RED;
+                break;
+            case COLOR_ORANGE:
+                toBeMatch.setImageResource(R.drawable.orange_footer);
+                matchColor = COLOR_ORANGE;
+                break;
+            case COLOR_YELLOW:
+                toBeMatch.setImageResource(R.drawable.yellow_footer);
+                matchColor = COLOR_YELLOW;
+                break;
+            case COLOR_GREEN:
+                toBeMatch.setImageResource(R.drawable.green_footer);
+                matchColor = COLOR_GREEN;
+                break;
+            case COLOR_BLUE:
+                toBeMatch.setImageResource(R.drawable.blue_footer);
+                matchColor = COLOR_BLUE;
+                break;
+            case COLOR_INDIGO:
+                toBeMatch.setImageResource(R.drawable.indigo1);
+                matchColor = COLOR_INDIGO;
+                break;
+            case COLOR_VIOLET:
+                toBeMatch.setImageResource(R.drawable.violet1);
+                matchColor = COLOR_VIOLET;
+                break;
+        }
+        return match;
+    }
+
+    //randomizes the colors around the board
+    private void generateGameBoard(int correct) {
+
+        int[] genBoardColor = { COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_BLUE, COLOR_INDIGO, COLOR_VIOLET};
+
+
+        switch (correct) {
+            case COLOR_RED:
+                btn_00.setImageResource(R.drawable.red1);
+                matchColor = COLOR_RED;
+
+                break;
+            case COLOR_ORANGE:
+                btn_00.setImageResource(R.drawable.orange1);
+                matchColor = COLOR_ORANGE;
+                break;
+            case COLOR_YELLOW:
+                btn_00.setImageResource(yellow1);
+                matchColor = COLOR_YELLOW;
+                break;
+            case COLOR_GREEN:
+                btn_00.setImageResource(R.drawable.green1);
+                matchColor = COLOR_GREEN;
+                break;
+            case COLOR_BLUE:
+                btn_00.setImageResource(R.drawable.blue1);
+                matchColor = COLOR_BLUE;
+                break;
+            case COLOR_INDIGO:
+                btn_00.setImageResource(R.drawable.indigo1);
+                matchColor = COLOR_INDIGO;
+                break;
+            case COLOR_VIOLET:
+                btn_00.setImageResource(R.drawable.violet1);
+                matchColor = COLOR_VIOLET;
+                break;
+        }
+
+    }
+
+    //randomizes the colors in an array
+    public static int[] randomColorArray (int [] array) {
+        Random rgen = new Random(); //generates random number
+
+        for (int i = 0; i <array.length; i++) {
+            int randomPosition = rgen.nextInt(array.length);
+            int temp = array[i];
+            array[i] = array[randomPosition];
+            array[randomPosition] = temp;
+        }
+
+        return array;
+    }
 
 
 
@@ -190,8 +260,9 @@ public class GameView extends AppCompatActivity {
 
 
     //correct color clicked method
-    public void correctColor(View v) {
-        btn_00.setVisibility(View.INVISIBLE);
+    public void correctColor() {
+        //btn_00.setVisibility(View.INVISIBLE);
+
     }
     //incorrect color clicked method
     public void incorrectColor(View v) {
